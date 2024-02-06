07be9a706cbc0f693be15ec8030b05bf61535576
gashpen <constantinent@mail.ru>
2024-02-06 18:25:48 +0300

    add:deploy workflow

--- src/component/data.js
+++ src/component/data.js
@@ -1,11 +1,11 @@
-
+import q from '../../src/assets/'
 
 const product = [
     {
         color: 'white',
         nameColorRu: 'Белый',
         colorHex: '#fff',
-        image: '../assets/905074.jpg',
+        image: '../../assets/905074.jpg',
         count: 200000,
         height: {
             height_200: 200,
@@ -17,7 +17,7 @@ const product = [
         color: 'nacre',
         nameColorRu: 'Перламутр',
         colorHex: '#D8CEC4',
-        image: '../assets/905075.jpg',
+        image: '../../assets/905075.jpg',
         count: 200000,
         markup: 5,
         height: {
@@ -30,7 +30,7 @@ const product = [
         color: 'darkGrey',
         nameColorRu: 'Темно-серый',
         colorHex: '#6E6E6E',
-        image: '../assets/905076.jpg',
+        image: '../../assets/905076.jpg',
         count: 200000,
         markup: 7,
         height: {
@@ -42,8 +42,8 @@ const product = [
     {
         color: 'sonoma',
         nameColorRu: 'Сонома',
-        image: '../assets/905077.jpg',
-        texture:'../assets/793731.jpg',
+        image: '../../assets/905077.jpg',
+        texture:'../../assets/793731.jpg',
         count: 200000,
         markup: 10,
         height: {
